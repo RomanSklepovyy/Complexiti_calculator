67b6eafa2a442a37938f62194b44d9807e74bfbc
roman <romansklepovyy1@gmail.com>
2020-03-03 22:52:33 +0200

    canvas bg started

---
+++ AnimationBackground.js
@@ -0,0 +1,12 @@
+(function () {
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    varw = canvas.width = innerWidth;
+    var h = canvas.height = innerHeight;
+    var particles = [];
+    var properties = {
+
+    }
+
+})();
